93c8934c43d1b1cca2048a856cb6da8602569034
Mohd Zaid <zaid.tab123@gmail.com>
2022-12-27 16:53:15 +0530

    🐛 optimize auth

--- src/api/posts/router.ts
+++ src/api/posts/router.ts
@@ -1,20 +1,17 @@
 import { Router, Request, Response } from 'express';
 import LoggerInstance from '../../loaders/logger';
-import { postBlog, updatePost } from './controller';
+import { deletePost, postBlog, updatePost } from './controller';
 import { blogPostValidator } from './validator';
 import { verifyToken } from '../../shared/helper/token';
 import ErrorClass from '../../shared/types/error';
 import { ObjectID } from 'mongodb';
+import authenticate from '../../shared/middleware/authentication';
 
 const blogPostRouter = Router();
 
 async function handleBlogPost(req: Request, res: Response) {
   try {
-    const id = verifyToken(req.headers.authorization?.split(' ')[1]);
-    if (!id) {
-      throw new ErrorClass('Unauthorized', 401);
-    }
-    const postBlogStatus = await postBlog(req.body, new ObjectID(id.id));
+    const postBlogStatus = await postBlog(req.body, res.locals.user);
     res.status(200).json({
       message: postBlogStatus.message,
     });
@@ -28,11 +25,7 @@ async function handleBlogPost(req: Request, res: Response) {
 
 async function handleUpdatePost(req: Request, res: Response) {
   try {
-    const UserID = await verifyToken(req.headers.authorization?.split(' ')[1]);
-    if (!UserID) {
-      throw new ErrorClass('Unauthorized', 401);
-    }
-    const updatePostStatus = await updatePost(req.params.id, req.body, UserID.id);
+    const updatePostStatus = await updatePost(req.params.id, req.body, res.locals.user);
     res.status(200).json({
       message: updatePostStatus.message,
     });
@@ -44,11 +37,22 @@ async function handleUpdatePost(req: Request, res: Response) {
   }
 }
 
-async function handleDeletePost(req:Request, res: Response) {
-  
+async function handleDeletePost(req: Request, res: Response) {
+  try {
+    const deletePostStatus = await deletePost(req.params.id, res.locals.user);
+    res.status(200).json({
+      message: deletePostStatus.message,
+    });
+  } catch (e) {
+    LoggerInstance.error(e);
+    res.status(e.status || 500).json({
+      message: e.message || "Post couldn't be deleted",
+    });
+  }
 }
 
-blogPostRouter.post('/', blogPostValidator, handleBlogPost);
-blogPostRouter.put('/:id', blogPostValidator, handleUpdatePost);
+blogPostRouter.post('/', blogPostValidator, authenticate, handleBlogPost);
+blogPostRouter.put('/:id', blogPostValidator, authenticate, handleUpdatePost);
+blogPostRouter.delete('/:id', blogPostValidator, authenticate, handleDeletePost);
 
 export default blogPostRouter;
